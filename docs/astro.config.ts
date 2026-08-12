eaaf15adf29f510cb13a4e92404518d05d4c14a9
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-12 14:53:21 -0700

    feat(docs): update Astro base to /projects/monorepo-template
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	site: "https://theholocron.github.io",
-	base: "/monorepo-template",
+	base: "/projects/monorepo-template",
 	integrations: [
 		starlight({
 			title: "Monorepo Template",
