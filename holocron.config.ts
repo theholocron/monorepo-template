ba9f90d486fda93d9f569853da71928f523c265e
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-25 10:52:02 -0700

    chore: 🔧 add sync workflow to holocron.config
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- holocron.config.ts
+++ holocron.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
 		...workflows,
 		{ name: "audit", with: { "run-knip": true } },
 		{ name: "release", with: { "run-build": true } },
+		"sync",
 		{ name: "deploy", with: { docs: true } },
 	],
 	providers: {
